d3c95545caa4537a05ed40adcd7740f5c2661deb
DeathFish0 <1273159606@qq.com>
2025-06-15 04:01:17 +0800

    Add Mowu title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="MOWU_Chat"
+        subTitle="MOWU AI assistant."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
